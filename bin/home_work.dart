ec5947a56d2b6124b08baedaa36436ee2c6b67a3
tekimbaev1 <tekimbaev@gmail.com>
2024-01-23 19:20:41 +0600

    first commit

---
+++ bin/home_work.dart
@@ -0,0 +1,11 @@
+void main() {
+  String myString = "Hello, Dart";
+  print(myString.runtimeType);
+
+  String myDart = "Dart programming language";
+  String dart = "dart";
+  print(myDart.contains(dart));
+
+  String dartIsFun = "dart is fun";
+  print(dartIsFun.toUpperCase());
+}
